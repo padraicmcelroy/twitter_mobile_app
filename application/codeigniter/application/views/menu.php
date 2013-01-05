91d18c3a0b6c7311c7bd0a71b715a4a2feb73e22
barryosull <barry@tercet.io>
2013-01-05 17:28:39 +0000

    Added the start of the Auth system. Login view created. Next: Add auth lib.

--- application/codeigniter/application/views/menu.php
+++ application/codeigniter/application/views/menu.php
@@ -1,3 +1,5 @@
+<?$active_menu_elem = isset($active_menu_elem)? $active_menu_elem: '';?>
+
   <div class="row-fluid">
     <div class="span12">
       <div class="navbar">
@@ -5,8 +7,15 @@
           <a class="brand" href="/">Tweetlist</a>
           <ul class="nav">
             <li <?=($active_menu_elem == 'my_tweets')? 'class="active"' :''?>><a href="/">My Tweets</a></li>
-            <li <?=($active_menu_elem == 'search')? 'class="active"' :''?>><a href="/search">Search</a></li>
+            <?if(isset($user)):?>
+              <li <?=($active_menu_elem == 'search')? 'class="active"' :''?>><a href="/search">Search</a></li>
+            <?endif;?>
           </ul>
+          <?if(isset($user)):?>
+            <span class="pull-right">Hello User | <a href="/auth/logout" class="btn">Logout</a></span>
+          <?else:?>
+            <a href="/auth/login" class="btn pull-right">Login</a>
+          <?endif;?>
         </div>
       </div>
     </div>
